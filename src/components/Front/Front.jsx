088da740f2631b716997fbba575ca0cbbc70a562
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-04-05 16:19:56 +0400

    front

--- src/components/Front/Front.jsx
+++ src/components/Front/Front.jsx
@@ -1,5 +1,24 @@
 import "./front.css";
+import sun from "/images/icon-star.svg";
 
 export default function Front() {
-  return <>jsjs</>;
+  let buttons = [1, 2, 3, 4, 5];
+  return (
+    <>
+      <div className="container">
+        <img src={sun} alt=" image of sun" className="sun" />
+        <h1>How did we do?</h1>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        <div className="buttons-con">
+          {buttons.map((el) => (
+            <button className="btn">{el}</button>
+          ))}
+        </div>
+        <button className="yelow">submit</button>
+      </div>
+    </>
+  );
 }
